4412aecfb4dc13b1e21128dbf0c384ce627d910a
FerraroJacopo <39092845+FerraroJacopo@users.noreply.github.com>
2021-12-07 16:23:22 +0100

    Update new.py

--- new.py
+++ new.py
@@ -1,131 +1,250 @@
-from telethon import TelegramClient, events, sync
+import socket, time
 from datetime import datetime
-import matplotlib.pyplot as plt
-import pandas as pd
-from pandas.plotting import register_matplotlib_converters
-register_matplotlib_converters()
-import MetaTrader5 as mt5
-import substring
+from functools import reduce
+
+def ConstructHeader(msg_type, SenderCompID, TargetCompID, SenderSubID, TargetSubID, messageSequenceNumber):
+    header = ''
+
+    # Version of FIX
+    header += "8=FIX.4.4|"
+
+    message = ''
+
+    # Type of message, A = String
+    message += "35=" + msg_type + "|"
+
+    message += "49="  + SenderCompID +  "|"
+    message += "56="  + TargetCompID +  "|"
+
+    message += "50="  + SenderSubID +  "|"
+    message += "57="  + TargetSubID +  "|"
+
+    message += "34="  + messageSequenceNumber +  "|"
+
+    utctimenow_year = str(datetime.utcnow().year)
+    utctimenow_month = str(datetime.utcnow().month)
+    utctimenow_date = str(datetime.utcnow().day)
+    utctimenow_date = utctimenow_date.zfill(2)
+
+    utctimenow_hour = str(datetime.utcnow().hour)
+    utctimenow_hour = utctimenow_hour.zfill(2)
+
+    utctimenow_min = str(datetime.utcnow().minute)
+    utctimenow_min = utctimenow_min.zfill(2)
+
+    utctime = utctimenow_year+utctimenow_month+utctimenow_date+'-'+utctimenow_hour+':'+utctimenow_min+':00'
+
+    message += "52="  + utctime +  "|"
+
+    #message += "52=20171204-16:20:00|"
+
+    return header, message
+
+def ConstructTrailer(headerAndBody):
+    headerAndBody = str(headerAndBody.replace('|', chr(0x01)))
+    chksum = str(reduce(lambda x,y:x+y, map(ord, headerAndBody)) % 256)
+    chksum = chksum.zfill(3)
+
+    return chksum
+    # expected 131
+
+def LogonMessage(heartBeatSeconds, username, password, resetSeqNum):
+    body = ''
+
+    # Encryption
+    body += "98=0|"
+
+    body += "108="  + heartBeatSeconds +  "|"
+
+    if resetSeqNum:
+        body += "141=Y|"
 
 
-#TELEGRAMI CREDENTIALS
-api_id = 8784760
-api_hash = 'c6b1db80accefe2574779d843d0d55b8'
-client = TelegramClient('session', api_id, api_hash)
+    body += "553=" + username + "|"
+    body += "554=" + password + "|"
 
-#FTMO METATRADER LOGIN CREDENTIALS
-mt5.initialize(
-   login=1056003464,             
-   password="6JR8DND4ZF",      
-   server="FTMO-Server")
+    return body
 
-#ACCOUNT EQUITY
-equity = 40000
- 
-# connect to MetaTrader 5
-if not mt5.initialize():
-    print("initialize() failed")
-    mt5.shutdown()
-else:
-    print("Bot in ascolto..")
+def LogonPacket():
+
+    # Header
+    msg_type = 'A'
+
+    SenderCompID = 'demo.icmarkets.8236701'
+    TargetCompID = 'cServer'
+
+    #SenderSubID = 'QUOTE'
+    SenderSubID = 'TRADE'
+    TargetSubID = 'TRADE'
+    messageSequenceNumber = '1'
+
+    header, message = ConstructHeader(msg_type, SenderCompID, TargetCompID, SenderSubID, TargetSubID, messageSequenceNumber)
+
+    # Body
+    heartBeatSeconds = '30'
+    username = '8236701'
+    password = 'provone'
+    resetSeqNum = 1
+
+    body = LogonMessage(heartBeatSeconds, username, password, resetSeqNum)
+
+    length = len(message) + len(body)
+    # FIX version + len of msg
+    header += "9=" + str(length) + "|"
+
+    # FIX version + len of msg + rest of the header
+    header = header + message
+
+    print('Header construction completed')
+    print('Header is: ', header, '\n')
+
+    # FIX version + len of msg + rest of the header + body (login)
+    headerAndBody = header + body
+
+    #headerAndBody = '8=FIX.4.4|9=126|35=A|49=theBroker.12345|56=CSERVER|34=1|52=20170117-08:03:04|57=TRADE|50=any_string|98=0|108=30|141=Y|553=12345|554=passw0rd!|'
+
+    # Trailer
+    chksum = ConstructTrailer(headerAndBody)
+    trailer = "10="+str(chksum)+"|"
+    #trailer = "10=254|"
+    # 254 QUOTE, 224 TRADE
+
+    #print('Trailer construction completed')
+    #print('Trailer is: ', trailer, '\n')
+
+    headerAndBodyAndTrailer = headerAndBody + trailer
+    #print('ctrader packet is: ', headerAndBodyAndTrailer, '\n')
+
+    headerAndBodyAndTrailer = headerAndBodyAndTrailer.replace("|", chr(0x01))
+
+    # ctrader packet constructor finished
+
+    return headerAndBodyAndTrailer
+
+def buy():
+
+    utctimenow_year = str(datetime.utcnow().year)
+    utctimenow_month = str(datetime.utcnow().month)
+    utctimenow_date = str(datetime.utcnow().day)
+    utctimenow_date = utctimenow_date.zfill(2)
+
+    utctimenow_hour = str(datetime.utcnow().hour)
+    utctimenow_hour = utctimenow_hour.zfill(2)
+
+    utctimenow_min = str(datetime.utcnow().minute)
+    utctimenow_min = utctimenow_min.zfill(2)
+
+    utctime = utctimenow_year+utctimenow_month+utctimenow_date+'-'+utctimenow_hour+':'+utctimenow_min+':00'
+
+    msg_type = 'D'
+
+    SenderCompID = 'demo.icmarkets.8236701'
+    TargetCompID = 'cServer'
+
+    #SenderSubID = 'QUOTE'
+    SenderSubID = 'TRADE'
+    TargetSubID = 'TRADE'
+    messageSequenceNumber = '2'
+
+    header, message = ConstructHeader(msg_type, SenderCompID, TargetCompID, SenderSubID, TargetSubID, messageSequenceNumber)
+
+    body = ""
+
+    body += "11="  + "1111" +  "|"
+
+    body += "55="  + "1" +  "|"
+
+    body += "54="  + "1" +  "|"
+
+    body += "60="  + utctime +  "|"
+
+    body += "38="  + "10000" +  "|"
+
+    body += "40="  + "1" +  "|"
+
+    length = len(message) + len(body)
+    # FIX version + len of msg
+    header += "9=" + str(length) + "|"
+
+    # FIX version + len of msg + rest of the header
+    header = header + message
+
+    print('Header construction completed')
+    print('Header is: ', header, '\n')
+
+    # FIX version + len of msg + rest of the header + body (login)
+    headerAndBody = header + body
+
+    #headerAndBody = '8=FIX.4.4|9=126|35=A|49=theBroker.12345|56=CSERVER|34=1|52=20170117-08:03:04|57=TRADE|50=any_string|98=0|108=30|141=Y|553=12345|554=passw0rd!|'
+
+    # Trailer
+    chksum = ConstructTrailer(headerAndBody)
+    trailer = "10="+str(chksum)+"|"
+    #trailer = "10=254|"
+    # 254 QUOTE, 224 TRADE
+
+    #print('Trailer construction completed')
+    #print('Trailer is: ', trailer, '\n')
+
+    headerAndBodyAndTrailer = headerAndBody + trailer
+    #print('ctrader packet is: ', headerAndBodyAndTrailer, '\n')
+
+    headerAndBodyAndTrailer = headerAndBodyAndTrailer.replace("|", chr(0x01))
+
+    # ctrader packet constructor finished
+
+    return headerAndBodyAndTrailer
 
 
+def Main():
+    host = 'h51.p.ctrader.com'
+    port = 5202
+  
+    api_id = 8784760
+    api_hash = 'c6b1db80accefe2574779d843d0d55b8'
+    client = TelegramClient('session', api_id, api_hash)
 
-def open_trade(action, symbol, sl_points, tp_points, price):
+    s = socket.socket()
+    s.connect((host, port))
 
-    # prepare the buy request structure
-    symbol_info = mt5.symbol_info(symbol)
-    trade_type=""
+    # Logon
+    seq_num = 1
+    headerAndBodyAndTrailer = LogonPacket()
+    #print(headerAndBodyAndTrailer)
+    #print('LOGON packet sent to ctrader!!!')
 
-    point = mt5.symbol_info(symbol).point
-
-    tick_value = mt5.symbol_info(symbol).trade_tick_value
-    tick_size = mt5.symbol_info(symbol).trade_tick_size
-
-    pipValue = (tick_value * point) / tick_size
-
-    if action == 'buy':
-        current_price = mt5.symbol_info_tick(symbol).ask
-        
-        if(price < current_price):
-            trade_type = mt5.ORDER_TYPE_BUY_LIMIT
-        else:
-            trade_type = mt5.ORDER_TYPE_BUY_STOP
-        
-        sl = price - sl_points * point
-        tp = price + tp_points * point
-        
-    elif action =='sell':
-        current_price = mt5.symbol_info_tick(symbol).bid
-
-        if(price > current_price):
-            trade_type = mt5.ORDER_TYPE_SELL_LIMIT
-        else:
-            trade_type = mt5.ORDER_TYPE_SELL_STOP
-            
-        sl = price + sl_points * point
-        tp = price - tp_points * point
-        
+    s.send(str.encode(headerAndBodyAndTrailer))
     
-    lot = (equity / (pipValue * sl_points))
-    lot = lot / 100
-    lot = round(lot, 2)
-    sl = round(sl, 4)
-    buy_request = {
-        "action": mt5.TRADE_ACTION_PENDING,
-        "symbol": symbol,
-        "volume": lot,
-        "type": trade_type,
-        "price": price,
-        "sl": sl,
-        "tp": tp,
-        "comment": "sent by python",
-        "type_time": mt5.ORDER_TIME_GTC, # good till cancelled
-        "type_filling": mt5.ORDER_FILLING_FOK,
-    }
-    # send a trading request
-    result = mt5.order_send(buy_request)
-    print(result)
-    return result, buy_request 
+    data = s.recv(1024)
+    data = str(data)
+    a = data[9]
+    data = data.replace(a, '|')
+    #print('\n\nLOGON RESP :  ', data)
+
+    seq_num = 2
+    headerAndBodyAndTrailer = buy()
+    print(headerAndBodyAndTrailer)
+    print('BUY packet sent to ctrader!!!')
+   
+    @client.on(events.NewMessage(chats="provone96"))
+    async def my_event_handler(event):
+      message = event.text
+      if "buy" in message:
+         s.send(str.encode(headerAndBodyAndTrailer))
+    client.start()
+    client.run_until_disconnected()
+    
+    data = s.recv(1024)
+    data = str(data)
+    a = data[9]
+    data = data.replace(a, '|')
+    print('\n\nBUY RESP :  ', data)
 
 
 
-#ON CHANNEL NEW MESSAGE
-@client.on(events.NewMessage(chats="provone96"))
-async def my_event_handler(event):
-    message = event.text
-    if "FREE SIGNAL" in message:
-        if "SELL" in message or "BUY" in message:
-            list_tmp = message.splitlines()
+if __name__ == '__main__':
+    Main()
 
-            list_final = []
-            for item in list_tmp:
-                list_final.extend(item.split())
-            list_final.remove("FREE")
-            list_final.remove("SIGNAL:")
-            list_final.remove("@")
-
-            action = str(list_final[0].lower())
-            symbol = str(list_final[1])
-            price = float(list_final[2])
-
-            multiplier = 100000
-            if "JPY" in symbol:
-                multiplier = 1000
-
-            take_profit = float(list_final[6])
-            tp = 0.0
-
-            if take_profit > price:
-                tp = (take_profit - price) * multiplier
-            else:
-                tp = (price - take_profit) * multiplier
-            sl = tp / 3
-
-            open_trade(action,symbol,sl,tp,price)
-
-client.start()
-client.run_until_disconnected()
 
 
 
